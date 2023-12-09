8e1c5aed003f32c4a46cc0f918c2004269bf9fdf
EdinhoNasciment <edson.nascimento@aluno.ifsp.edu.br>
2023-12-08 20:51:21 -0800

    Tela de login com confirmação de OTP e Username feita

---
+++ app/src/main/java/br/edu/ifsp/mindsapp/LoginOtpActivity.java
@@ -0,0 +1,14 @@
+package br.edu.ifsp.mindsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginOtpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_otp);
+    }
+}
